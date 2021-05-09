4b44c588c4b854e08d13dafbee10be6408e5798f
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2021-05-09 00:10:56 -0300

    clean index code

--- index.js
+++ index.js
@@ -7,27 +7,19 @@ document.addEventListener('DOMContentLoaded', function () {
         pets: ''
     }
 
-    let form, sunSelect, waterSelect, petsSelect
-
-    form = document.getElementsByClassName('main--searchFilters__form')[0]
+    let form = document.getElementsByClassName('main--searchFilters__form')[0]
     form.addEventListener('submit', event => event.preventDefault())
 
     const scrollToTopBtn = document.getElementsByClassName('main--results__container--scrollToTopBtn')[0]
     scrollToTopBtn.addEventListener('click', () => scrollToTop())
 
-    sunSelect = document.getElementsByClassName('main--searchFilters__form--sun__select')[0]
-    waterSelect = document.getElementsByClassName('main--searchFilters__form--water__select')[0]
-    petsSelect = document.getElementsByClassName('main--searchFilters__form--pets__select')[0]
-    sunSelect.addEventListener('change', event => {
-        requestParams.sun = event.target.value
-        handleSelectChange(requestParams)
-    })
-    waterSelect.addEventListener('change', event => {
-        requestParams.water = event.target.value
-        handleSelectChange(requestParams)
-    })
-    petsSelect.addEventListener('change', event => {
-        requestParams.pets = event.target.value
-        handleSelectChange(requestParams)
+    const elementsArray = Object.keys(requestParams)
+    elementsArray.forEach(element => {
+        const node = document.getElementsByClassName(`main--searchFilters__form--${element}__select`)[0]
+
+        node.addEventListener('change', event => {
+            requestParams[element] = event.target.value
+            handleSelectChange(requestParams)
+        })
     })
 })
