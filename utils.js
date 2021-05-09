386ce20ee56ee701d0610262a13dde4aa47d2197
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2021-05-09 00:16:08 -0300

    remove unecessary conditional

--- utils.js
+++ utils.js
@@ -51,11 +51,10 @@ function toggleResults(plantsArray) {
 async function fetchPlantResults(requestParams) {
     const { sun, water, pets } = requestParams
     let result
-    if (sun && water && pets) {
-        await fetch(`https://front-br-challenges.web.app/api/v2/green-thumb/?sun=${sun}&water=${water}&pets=${pets}`)
-            .then(response => response.json())
-            .then(data => (result = data))
-    }
+
+    await fetch(`https://front-br-challenges.web.app/api/v2/green-thumb/?sun=${sun}&water=${water}&pets=${pets}`)
+        .then(response => response.json())
+        .then(data => (result = data))
 
     return result
 }
